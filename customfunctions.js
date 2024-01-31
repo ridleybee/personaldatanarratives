3fe9f803637d927bbc29bd9d5e57607e1cd8832f
ridleybee <114196746+ridleybee@users.noreply.github.com>
2024-01-31 16:06:11 -0600

    update index and adding new sketch

---
+++ customfunctions.js
@@ -0,0 +1,56 @@
+
+let squareBrushBool = false
+let ellipseBrushBool = false
+
+function setup(){
+
+	createCanvas(windowWidth,windowHeight)
+	background(165, 185, 150)
+	rectMode(CENTER)
+
+}
+
+function draw(){
+
+	noStroke()
+
+	if (squareBrushBool == true)
+	{
+		squareBrush()
+	}
+
+	if(ellipseBrushBool == true){
+		ellipseBrush()
+	}
+
+
+}
+
+
+function ellipseBrush(){
+
+	fill(140,35,240)
+	ellipse(mouseX,mouseY, 50,50)
+}
+
+
+function squareBrush(){
+
+	fill(235,140,40)
+	rect(mouseX,mouseY,20,20)
+
+}
+
+
+function keyPressed(){
+	if(key=='s'){
+		squareBrushBool = true
+		ellipseBrushBool = false
+	}
+
+	if(key=='e'){
+		squareBrushBool = false
+		ellipseBrushBool = true
+	}
+
+}
